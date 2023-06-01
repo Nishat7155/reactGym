27e44ab8fe6e48a668c7c3c39e25cbb07ad89218
Nishat7155 <nishat_lisa@yahoo.com>
2023-06-01 16:56:00 +0200

    components
    components and styles are added

---
+++ gymwebsite/src/components/Hero/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Container, MainHeading } from '../../globalStyles';
+import { HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton } from './HeroStyles';
+
+const Hero = () => {
+	return (
+		<HeroSection>
+			<HeroVideo src="./assets/g4.mp4" autoPlay muted />
+			<Container>
+				<MainHeading>Fit body, fit mind, fit life</MainHeading>
+				<HeroText>
+					Fitness is not about being better than someone else, it’s about being better than you used to be
+				</HeroText>
+				<ButtonWrapper>
+					<Link to="signup">
+						<Button>Get Started</Button>
+					</Link>
+					<HeroButton>Find More</HeroButton>
+				</ButtonWrapper>
+			</Container>
+		</HeroSection>
+	);
+};
+
+export default Hero;
---
+++ gymwebsite/src/data/PricingData.js
@@ -0,0 +1,42 @@
+export const pricingData = [
+	{
+		title: 'Basic',
+		price: '300 kr user/month ',
+		features: [
+			'Access to your favorite gym',
+      'Access to group training at your favorite gym',
+      'A free trial session with a personal trainer',
+      'Access to several gyms- NOT applicable',
+      'Access to all our gyms in the Nordics- NOT applicable',
+      'iFit Online -NOT applicable' ,
+			],
+	},
+	{
+		title: 'Standard',
+		price: '500 kr user/month ',
+		
+		features: [
+			'Access to all gyms in your region',
+      'Access to all of our 132 group exercise classes',
+      'A free trial session with a personal trainer',
+      'Access to iFit Online',
+      'Access to all our gyms in the Nordics - NOT applicable',
+		],
+	},
+	{
+		title: 'Premium',
+		price: '750 kr user/ month',
+		features: [
+			'Access to all our gyms in the Nordics',  
+      'Access to all of our 132 group exercise classes',
+      'A free trial session with a personal trainer',
+      'Online included in your membership' ,
+      'Babysitting at kids care',
+      'Book classes 14 days in advance',
+      '1 month membership freeze',
+			
+		],
+	},
+
+	
+];
